2b2722f36d505e7b7a122ad74e7f0e7b5374e5d0
nijatmammadovv <tu7q78xlv@code.edu.az>
2022-03-15 16:50:11 +0400

    Add project files.

---
+++ Weapon Task111/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Weapon_Task;
+
+namespace Weapon_Task11
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("WELCOME");
+            Console.WriteLine("Write the bullet capacity");
+            int bulletCapacity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Record the number of bullets");
+            int bulletcount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Record the release time");
+            double time = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Write the shooting mode");
+
+            Weapon Gun = new Weapon(25, 12, 5, 1);
+
+            int choice = 0;
+            do
+            {
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Gun.Shoot();
+                        break;
+                    case 2:
+                        Gun.Fire();
+                        break;
+                    case 3:
+                        Gun.GetRemainBulletCount();
+                        break;
+                    case 4:
+                        Gun.Reload();
+                        break;
+                    case 5:
+                        Console.WriteLine("Thank you");
+                        break;
+                    case 0:
+                        Gun.GetInfo();
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Use the numbers 1,2,3,4,5,0");
+                        break;
+                        
+                }
+            } while (choice != 5);
+        }
+    }
+}
---
+++ Weapon Task111/Weapon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weapon_Task
+{
+    class Weapon
+    {
+        public int bulletCapacity;
+        public int bulletCount;
+        public double dischargeSeconds;
+        public int fireMood;
+
+
+        public Weapon(int bulletCapacity, int bulletCount, double dischargeSeconds, int fireMood)
+        {
+            this.bulletCapacity = bulletCapacity;
+            this.bulletCapacity = bulletCount;
+            this.dischargeSeconds = dischargeSeconds;
+            this.fireMood = fireMood;
+
+        }
+
+
+        public void Shoot()
+        {
+            if (bulletCount > 0)
+            {
+                bulletCount--;
+                Console.WriteLine("1 bullet was fired");
+            }
+            else
+            {
+                Console.WriteLine("no bullet left");
+            }
+        }
+        public void Fire()
+        {
+            if (bulletCount == 0)
+            {
+                Console.WriteLine("no bullets");
+            }
+            else
+            {
+                double time = bulletCount * dischargeSeconds / bulletCapacity;
+                Console.WriteLine(time + "san");
+            }
+        }
+
+        public void GetRemainBulletCount()
+        {
+            int required = 0;
+            required = bulletCapacity - bulletCount;
+            Console.WriteLine("How many bullets are left:" + required);
+        }
+        public void Reload()
+        {
+            if (bulletCapacity - bulletCount == 0)
+            {
+                Console.WriteLine("the comb of the bullet is full");
+            }
+            else
+            {
+                Console.WriteLine($"{bulletCapacity - bulletCount}: + n add as many bullets");
+            }
+        }
+        public void GetInfo()
+        {
+
+        }
+    }
+}
+
